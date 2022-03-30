ee6b95e492fdd07a756894703bf23d817c45d6c1
Nguyen Ngoc Cong <ngoccong.nncpro@gmail.com>
2022-03-30 16:38:06 +0700

    chapter5: create seeders, factories

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //create first admin
+        $user = User::create([
+            "first_name" => "Công",
+            "last_name" => "Nguyễn Ngọc",
+            "email" => "congnn@gmail.com",
+            "username" => "nguyenngoccong",
+            "password" => Hash::make("123456"),
+        ]);
+        $user->is_admin = 1;
+        $user->save();
+
+        //create 10 users by factory and attach 10 notes for each user
+        factory(User::class, 10)->create()->each(function ($user) {
+            $user->notes()->saveMany(factory(Note::class, 10)->make());
+        });
+    }
+}
